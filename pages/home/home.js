ab811ea7040559662915d2218eb64dc6c77fad61
Hufan-MY <5299584+hufanmy@user.noreply.gitee.com>
2020-03-16 19:03:35 +0800

    1917_展示弹窗和网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,46 @@
 // pages/home/home.js
+import request from "../../service/request.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  onLoad: () => {
+    wx.showShareMenu({
+      withShareTicket: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnTap() {
+    request({
+        url: 'http://123.207.32.32:8000/api/wh/recommend'
+      })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log("出错啦")
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleToastTap() {
+    wx.showToast({
+      title:"加载中...",
+      duration:3000,
+      mask:true,
+      icon:"loading"
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleModalTap(){
+    wx.showModal({
+      title: '确定嘛',
+      content: 'lalal',
+      success:(res)=>{
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleLoadingTap(){
+    wx.showLoading({
+      title: '加载中...',
+      mask:true
+    })
+    setTimeout(()=>{
+      wx.hideLoading()
+    },1000)
   }
 })
