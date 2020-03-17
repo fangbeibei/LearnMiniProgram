c81866765918f4087a1e0862c81dce87b0944af2
Hufan-MY <5299584+hufanmy@user.noreply.gitee.com>
2020-03-18 01:15:51 +0800

    19_页面跳转

--- pages/about/about.js
+++ pages/about/about.js
@@ -7,60 +7,24 @@ Page({
   data: {
 
   },
-
+  handleBack(){
+    wx.navigateBack({
+      delta: 1,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onUnload: function(options){
+    // 每次页面跳转都会将页面压栈，这样就可以通过栈操作返回，也可以获取前面的页面，并向前面的页面传递数据
+    const pages = getCurrentPages()
+    console.log(pages)
+    const home = pages[pages.length - 2]
+    home.setData({
+      name:"hunaonao"
+    })
   }
 })
